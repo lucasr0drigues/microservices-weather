d01d8abaef86ea31ea2f2d40c22d752773551912
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-06-30 14:27:05 -0300

    add data access to precipitation service

--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,12 +1,33 @@
+using CloudWeather.Precipitation.DataAccess;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<PrecipitationDbContext>(
+    opt =>
+    {
+        opt.EnableSensitiveDataLogging();
+        opt.EnableDetailedErrors();
+        opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+    }, ServiceLifetime.Transient
+);
 
 // Add services to the container.
 var app = builder.Build();
 
-app.MapGet("/observation/{zip}", (string zip, [FromQuery] int? days) => {
-    return Results.Ok(zip); 
+app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, PrecipitationDbContext db) => {
+    if (days == null || days < 1 || days > 30)
+    {
+        return Results.BadRequest("Please provide a 'days' query parameter between 1 and 30");
+    }
+    var startDate = DateTime.UtcNow.AddDays((days.Value * -1));
+    var startDate2 = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+    var results = await db.Precipitation
+        .Where(precip => precip.ZipCode == zip && precip.CreatedOn > startDate)
+        .ToListAsync();
+
+    return Results.Ok(results);
 });
 
 app.Run();
