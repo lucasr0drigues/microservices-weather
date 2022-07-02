2e15b08af25701fe6fc8b62145f233e676395021
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-07-02 17:40:39 -0300

    build initial data loader

---
+++ CloudWeather.DataLoader/Models/TemperatureModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudWeather.DataLoader.Models
+{
+    public class TemperatureModel
+    {
+        public DateTime CreatedOn { get; set; }
+        public decimal AmountInches { get; set; }
+        public string WeatherType { get; set; }
+        public string ZipCode { get; set; }
+        public decimal TempLowF { get; set; }
+        public decimal TempHighF { get; set; }
+
+    }
+}
